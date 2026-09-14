e937c2cf322142f5b733b750fddf97e29241b1fd
Aaniya Jaiswal <aaniyajaiswal63@gmail.com>
2026-09-14 21:48:26 +0530

    chore: configure static files for deployment

--- myproject/settings.py
+++ myproject/settings.py
@@ -123,6 +123,7 @@ USE_TZ = True
 STATIC_DIR=BASE_DIR/'static'
 STATIC_URL='/static/'
 STATICFILES_DIRS=[STATIC_DIR]
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
